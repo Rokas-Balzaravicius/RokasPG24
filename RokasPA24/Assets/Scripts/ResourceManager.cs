750551a1f565398497c8f069c64d40b3175888e2
STUDENT Rokas Balzaravicius <Rokas.Balzaravicius@students.ittralee.ie>
2024-01-26 10:54:44 +0000

    Signed-off-by: STUDENT Rokas Balzaravicius <Rokas.Balzaravicius@students.ittralee.ie>

---
+++ RokasPA24/Assets/Scripts/ResourceManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourceManager : MonoBehaviour
+{
+
+    public Transform treeCloneTemplate;
+    int NUMBER_OF_TREES = 100;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i = 0; i < NUMBER_OF_TREES; i++) 
+        {
+            Instantiate(treeCloneTemplate, getTreeSpawnLocation(), Quaternion.identity);
+        }
+    }
+
+    private Vector3 getTreeSpawnLocation() 
+    {
+        return new Vector3(UnityEngine.Random.Range(-100f, 100f), 0, UnityEngine.Random.Range(-100f, 100f));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
