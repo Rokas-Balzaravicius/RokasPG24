e299986ac6a1c5742637a3a52309c7b98fe82445
STUDENT Rokas Balzaravicius <Rokas.Balzaravicius@students.ittralee.ie>
2024-01-30 15:57:27 +0000

    Signed-off-by: STUDENT Rokas Balzaravicius <Rokas.Balzaravicius@students.ittralee.ie>

--- RokasPA24/Assets/Scripts/PlayerMovement.cs
+++ RokasPA24/Assets/Scripts/PlayerMovement.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
@@ -7,12 +8,17 @@ public class PlayerMovement : MonoBehaviour
     float walkingspeed = 3f;
     float runningspeed = 5f;
     Animator PlayerAnimator;
+
     float lookingspeed = 2f;
+
+   
+    
+   
     void Start()
     {
         PlayerAnimator = GetComponentInChildren<Animator>();
-        
-
+       
+       
     }
 
     // Update is called once per frame
@@ -20,48 +26,68 @@ public class PlayerMovement : MonoBehaviour
     {
        
 
-
         PlayerAnimator.SetBool("isWalking", false);
+        PlayerAnimator.SetBool("isInspect", false);
 
-        if(Input.GetKey(KeyCode.W)) 
+        if (Input.GetKey(KeyCode.W))
         {
             transform.position += walkingspeed * transform.forward * Time.deltaTime;
             PlayerAnimator.SetBool("isWalking", true);
-
+            
         }
+
+      
 
 
         if (Input.GetKey(KeyCode.S))
         {
             transform.position -= walkingspeed * transform.forward * Time.deltaTime;
             PlayerAnimator.SetBool("isWalking", true);
-
+           
         }
+      
 
         if ( Input.GetKey(KeyCode.D))
         {
             transform.position += walkingspeed * transform.right * Time.deltaTime;
             PlayerAnimator.SetBool("isWalking", true);
+            
 
         }
+
+       
 
         if (Input .GetKey(KeyCode.A)) 
         {
             transform.position -= walkingspeed * transform.right * Time.deltaTime;
             PlayerAnimator.SetBool("isWalking", true);
-
+            
         }
+
+     
 
         if (Input.GetKey(KeyCode.LeftShift))
         {
             transform.position += runningspeed * transform.forward * Time.deltaTime;
             PlayerAnimator.SetBool("isRunning", true);
+            
         }
 
         else
         {
             PlayerAnimator.SetBool("isRunning", false);
+           
         }
 
+        if(Input.GetKey(KeyCode.F)) 
+        {
+            PlayerAnimator.SetBool("isInspect", true);
+        }
+
+        
+
+        
     }
+
+    
 }
