9429f8e2ac2c808992dc12761a803179f8fe200c
STUDENT Rokas Balzaravicius <Rokas.Balzaravicius@students.ittralee.ie>
2024-02-09 10:51:46 +0000

    Signed-off-by: STUDENT Rokas Balzaravicius <Rokas.Balzaravicius@students.ittralee.ie>

--- RokasPA24/Assets/Scripts/PlayerMovement.cs
+++ RokasPA24/Assets/Scripts/PlayerMovement.cs
@@ -1,30 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
+using UnityEditor.Timeline;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     float walkingspeed = 3f;
     float runningspeed = 5f;
+    float crouchspeed = 2f;
     Animator PlayerAnimator;
-    
-    float lookingspeed = 2f;
 
-   
-    
-   
+
+    float lookingspeed = 2f;
+    private bool isCrouching;
+    private Vector3 orignalCenter;
+    private float orignalHeight;
+    private float originalMoveSpeed;
+
+
     void Start()
     {
         PlayerAnimator = GetComponentInChildren<Animator>();
-       
-       
+        transform.tag = "Player";
+        orignalCenter = transform.localPosition;
+        orignalHeight = transform.localScale.y;
+        originalMoveSpeed = walkingspeed;
+        
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-       
+
 
         PlayerAnimator.SetBool("isWalking", false);
         PlayerAnimator.SetBool("isInspect", false);
@@ -35,67 +45,67 @@ public class PlayerMovement : MonoBehaviour
 
 
 
-       
+
         if (Input.GetKey(KeyCode.W))
         {
             transform.position += walkingspeed * transform.forward * Time.deltaTime;
             PlayerAnimator.SetBool("isWalking", true);
-            
+
         }
 
-      
+
 
 
         if (Input.GetKey(KeyCode.S))
         {
             transform.position -= walkingspeed * transform.forward * Time.deltaTime;
             PlayerAnimator.SetBool("isWalking", true);
-           
-        }
-      
 
-        if ( Input.GetKey(KeyCode.D))
+        }
+
+
+        if (Input.GetKey(KeyCode.D))
         {
             transform.position += walkingspeed * transform.right * Time.deltaTime;
             PlayerAnimator.SetBool("isWalking", true);
-            
+
 
         }
 
-       
 
-        if (Input .GetKey(KeyCode.A)) 
+
+        if (Input.GetKey(KeyCode.A))
         {
             transform.position -= walkingspeed * transform.right * Time.deltaTime;
             PlayerAnimator.SetBool("isWalking", true);
-            
+
         }
 
-     
+
 
         if (Input.GetKey(KeyCode.LeftShift))
         {
             transform.position += runningspeed * transform.forward * Time.deltaTime;
             PlayerAnimator.SetBool("isRunning", true);
-            
+
         }
 
         else
         {
             PlayerAnimator.SetBool("isRunning", false);
-           
+
         }
 
-        if(Input.GetKey(KeyCode.F)) 
+        if (Input.GetKey(KeyCode.F))
         {
             PlayerAnimator.SetBool("isInspect", true);
         }
 
 
-        if(Input.GetKey (KeyCode.R)) 
+        if (Input.GetKey(KeyCode.R))
         {
             PlayerAnimator.SetBool("isReload", true);
-            
+
 
         }
 
@@ -104,30 +114,25 @@ public class PlayerMovement : MonoBehaviour
             PlayerAnimator.SetBool("isFire", true);
             Ray gunshot = new Ray(transform.position, transform.forward);
             Debug.DrawRay(gunshot.origin, gunshot.direction * 50, Color.red, 5);
-            if (Physics.Raycast(gunshot,50))
+            if (Physics.Raycast(gunshot, 50))
                 print("I hit something");
 
 
         }
 
-        if( Input.GetKey(KeyCode.Mouse1))
+        if (Input.GetKey(KeyCode.Mouse1))
         {
             PlayerAnimator.SetBool("isAim", true);
         }
 
-       /* if (Input.GetKey(KeyCode.LeftControl)) 
+       /* if(Input.GetKeyDown(KeyCode.LeftControl))
         {
-            transform.position = Vector3.down / 2;
-        }
-        else
-        {
-            transform.position = Vector3.down * 2;
+            transform.position = Vector3.down * 1f;
+            transform.position += crouchspeed*T
+            
         }*/
 
-        
 
-        
+
     }
-
-    
 }
---
+++ RokasPA24/Assets/Scripts/StartUpUI.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartUpUI : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+        print("Exiting game");
+    }
+
+    public void StartGame()
+    {
+        //Start Game
+        SceneManager.LoadScene(1);
+        print("Playing game");
+    }
+
+
+}
