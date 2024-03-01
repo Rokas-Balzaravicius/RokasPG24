173b8c82226897e9c3a63aefec1377cd06431392
STUDENT Rokas Balzaravicius <Rokas.Balzaravicius@students.ittralee.ie>
2024-03-01 10:52:37 +0000

    Signed-off-by: STUDENT Rokas Balzaravicius <Rokas.Balzaravicius@students.ittralee.ie>

--- RokasPA24/Assets/Scripts/ResourceManager.cs
+++ RokasPA24/Assets/Scripts/ResourceManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,6 +8,9 @@ public class ResourceManager : MonoBehaviour
 
     public Transform treeCloneTemplate;
     int NUMBER_OF_TREES = 100;
+
+    public Transform ZombieClone;
+    int NumberOfZombies = 10;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +18,16 @@ public class ResourceManager : MonoBehaviour
         {
             Instantiate(treeCloneTemplate, getTreeSpawnLocation(), Quaternion.identity);
         }
+
+        for(int i = 0;i < NumberOfZombies; i++)
+        {
+            Instantiate(ZombieClone, getZombieLocation(), Quaternion.identity);
+        }
+    }
+
+    private Vector3 getZombieLocation()
+    {
+        return new Vector3(UnityEngine.Random.Range(-200f, 200f), 0, UnityEngine.Random.Range(-100f, 100f));
     }
 
     private Vector3 getTreeSpawnLocation() 
